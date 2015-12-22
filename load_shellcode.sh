70ba792478282a84b50065b82403f610db1b85f6
Evgeny Nosov <Evgeny.Nosov@Symphonyteleca.com>
2015-12-22 13:11:02 +0300

    sed -i

--- load_shellcode.sh
+++ load_shellcode.sh
@@ -15,10 +15,10 @@ offset="0x"$(readelf -S $outfile | grep .text | awk '{print $6}')
 size=$(size -A -d $outfile | grep .text | awk '{print $2}')
 echo "Size of section: $size"
 hex=$(hexdump -v -s $offset -n $size -e '"\\\\""x" 1/1 "%02x"' $outfile)
-sed "s@\(^char code[^\"]*\)\"\(.*\)\"@\1\"$hex\"@" $2 > $tmpfile
+sed -i "s@\(^char code[^\"]*\)\"\(.*\)\"@\1\"$hex\"@" $2 # > $tmpfile
 
-rm $2
-mv $tmpfile $2
+#rm $2
+#mv $tmpfile $2
 
 cc -m32 -fno-stack-protector -z execstack $2
 
